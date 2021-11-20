c099a1cb271fd0346eadfe1e5f4164e0d5bd2714
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2021-11-20 17:01:43 -0300

    Input control

--- Callbacks.h
+++ Callbacks.h
@@ -3,5 +3,5 @@
 #include <GLFW/glfw3.h>
 
 void SetCallbacks(GLFWwindow* window);
-void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+//void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@
 #include "Shader.h"
 #include "GPUData.h"
 #include "Texture.h"
+#include "Input.h"
 
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+Input g_Input;
+
 void SetupGLFW();
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 
 int main()
 {
@@ -27,7 +35,12 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
-	SetCallbacks(window);
+	//SetCallbacks(window);
+
+	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetMouseButtonCallback(window, MouseButtonCallback);
+	glfwSetCursorPosCallback(window, CursorPosCallback);
+	glfwSetScrollCallback(window, ScrollCallback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -44,11 +57,15 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		if (g_Input.GetKeyState(GLFW_KEY_ESCAPE).is_pressed)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+
 		ClearScreenBuffers();
 		glBindVertexArray(VAOs[LenticularObjectVAO]);
 		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 
 		glfwSwapBuffers(window);
+		g_Input.Update();
 		glfwPollEvents();
 	}
 
@@ -65,4 +82,29 @@ void SetupGLFW()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
+
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
+{
+	g_Input.KeyCallback(key, action, mod);
+}
+
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	g_Input.KeyCallback(button, action, mods);
+}
+
+void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	g_Input.cursor_state.xvalue = xpos;
+	g_Input.cursor_state.yvalue = ypos;
+	g_Input.cursor_changed = true;
+}
+
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	g_Input.scroll_state.xvalue = xoffset;
+	g_Input.scroll_state.yvalue = yoffset;
+	g_Input.scroll_changed = true;
 }
